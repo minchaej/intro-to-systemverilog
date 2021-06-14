9ae2d9466b0ab3ed66fc579dc192415613a09ad2
Nathan Jeong <nathan@funnc.com>
2021-06-14 20:28:44 +0900

    fix

--- section_1/module_b.sv
+++ section_1/module_b.sv
@@ -12,4 +12,13 @@ always_comb begin
   y3 = a ^ b;
 end
 
+/*
+could also be done with a non-blocking assigment <=
+always_comb begin
+  y1 <= a & b;
+  y2 <= a | b;
+  y3 <= a ^ b;
+end
+*/
+
 endmodule
